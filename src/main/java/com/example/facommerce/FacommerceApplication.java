e04015fc6481089d69f58b55eb65bcba0e928494
Gustavo Lopez Flores <202319070920@lab03-l>
2024-04-30 11:24:53 -0400

    estrutura base

---
+++ src/main/java/com/example/facommerce/FacommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.facommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/facommerce/FacommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
