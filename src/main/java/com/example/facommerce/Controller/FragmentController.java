9b20c9d8fd143d12859fed34c8494dc330b49a66
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 16:01:52 -0400

    criação de FragmentControllder

---
+++ src/main/java/com/example/facommerce/Controller/FragmentController.java
@@ -0,0 +1,13 @@
+package com.example.facommerce.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class FragmentController {
+    @GetMapping("/fragments/general")
+    public String header() {
+        return "/fragments/general.html";
+    }
+}
