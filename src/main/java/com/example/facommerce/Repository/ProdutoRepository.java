a69d7e00970bda4ae98b745b22656f7805e64c4e
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 21:21:51 -0400

    ProdutoRepository em Repository

---
+++ src/main/java/com/example/facommerce/Repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.example.facommerce.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends CrudRepository<Produto, Long> {
+  
+}
