ba36ee90fe331e735831d7e406518272534806ab
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 21:53:57 -0400

    Entity Categoria

---
+++ src/main/java/com/example/facommerce/Model/Categoria.java
@@ -0,0 +1,54 @@
+package com.example.facommerce.Model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Produto> produtos;
+
+    public Categoria() {}
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+}
