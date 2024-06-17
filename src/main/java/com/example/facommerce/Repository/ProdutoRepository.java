8fd0279112c24bb98ae01f01845e0a8a9d1ca745
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 21:58:36 -0400

    Adição de import Model.Produto

--- src/main/java/com/example/facommerce/Repository/ProdutoRepository.java
+++ src/main/java/com/example/facommerce/Repository/ProdutoRepository.java
@@ -3,6 +3,8 @@ package com.example.facommerce.Repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.facommerce.Model.Produto;
+
 @Repository
 public interface ProdutoRepository extends CrudRepository<Produto, Long> {
   
