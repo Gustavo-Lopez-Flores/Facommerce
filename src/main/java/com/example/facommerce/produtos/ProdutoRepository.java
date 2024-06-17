bec55bbbb4b1e6bf4be652671a1b8ecf5aebcb1e
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 21:20:41 -0400

    Mudança de package Produtos para Model, Repository e Service

--- src/main/java/com/example/facommerce/produtos/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.facommerce.produtos;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends CrudRepository<Produto, Long> {
-  
-}
