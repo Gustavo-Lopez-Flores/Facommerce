add77fc874237908d3671c42f0e1b70d81dba469
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 21:53:43 -0400

    Entity Carrinho

---
+++ src/main/java/com/example/facommerce/Model/Carrinho.java
@@ -0,0 +1,64 @@
+package com.example.facommerce.Model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "carrinhos")
+public class Carrinho {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @Column(nullable = false)
+    private LocalDateTime dataCriacao;
+
+    @OneToMany(mappedBy = "carrinho", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemCarrinho> itens;
+
+    public Carrinho() {}
+
+    public Carrinho(Usuario usuario, LocalDateTime dataCriacao) {
+        this.usuario = usuario;
+        this.dataCriacao = dataCriacao;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public List<ItemCarrinho> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemCarrinho> itens) {
+        this.itens = itens;
+    }
+}
