7eb4d5cb187b3e2588de3a6ebc8951f0e1cb6f85
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-23 23:10:36 -0400

    eliminação do construtor

--- src/main/java/com/example/facommerce/Controller/HomeController.java
+++ src/main/java/com/example/facommerce/Controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.facommerce.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -21,11 +22,6 @@ public class HomeController {
 
     @Autowired
     private CategoriaService categoriaService;
-
-    public HomeController(ProdutoService produtoService, CategoriaService categoriaService) {
-        this.produtoService = produtoService;
-        this.categoriaService = categoriaService;
-    }
 
     @GetMapping("/")
     public ModelAndView home(Model model) {
