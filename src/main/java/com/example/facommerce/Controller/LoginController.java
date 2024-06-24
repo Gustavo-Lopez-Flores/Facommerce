a71bc4fcfdd5667389e14d40303bd8f13c86d980
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-23 23:46:56 -0400

    request param com valores default

--- src/main/java/com/example/facommerce/Controller/LoginController.java
+++ src/main/java/com/example/facommerce/Controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.facommerce.DTO.LoginDTO;
@@ -26,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ModelAndView logar(@ModelAttribute LoginDTO loginDTO, boolean lembrar, HttpServletResponse response) {
+    public ModelAndView logar(@ModelAttribute LoginDTO loginDTO, @RequestParam(name = "lembrar", defaultValue = "false") boolean lembrar, HttpServletResponse response) {
         Usuario usuario = usuarioService.login(loginDTO.getEmail(), loginDTO.getSenha());
         if (usuario != null) {
             int maxAge = lembrar ? 60 * 60 * 24 * 30 : 60 * 60;
