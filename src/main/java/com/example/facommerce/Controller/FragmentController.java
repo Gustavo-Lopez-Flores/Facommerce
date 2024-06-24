b906c6d96710525abb7c2558c56d9f971e54d990
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-23 22:58:51 -0400

    header com if para adicionar usuarioId e tipoUsuario ao modelo, se não estiverem presentes

--- src/main/java/com/example/facommerce/Controller/FragmentController.java
+++ src/main/java/com/example/facommerce/Controller/FragmentController.java
@@ -1,6 +1,7 @@
 package com.example.facommerce.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @Controller
 @SessionAttributes({"usuarioId", "tipoUsuario"})
 public class FragmentController {
-
+    
     @GetMapping("/fragments/general")
-    public String header(@SessionAttribute("usuarioId") String usuarioId, @SessionAttribute("tipoUsuario") String tipoUsuario) {
-        return "/fragments/general.html";
+    public String header(Model model) {
+        if (!model.containsAttribute("usuarioId")) {
+            model.addAttribute("usuarioId", null);
+        }
+        if (!model.containsAttribute("tipoUsuario")) {
+            model.addAttribute("tipoUsuario", null);
+        }
+        return "fragments/general.html";
     }
 }
