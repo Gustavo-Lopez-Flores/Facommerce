7deed5c68e041bfb8582684cc407070859fba229
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-23 23:14:36 -0400

    Simplificação do fragmentController

--- src/main/java/com/example/facommerce/Controller/FragmentController.java
+++ src/main/java/com/example/facommerce/Controller/FragmentController.java
@@ -1,23 +1,12 @@
 package com.example.facommerce.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@SessionAttributes({"usuarioId", "tipoUsuario"})
 public class FragmentController {
-    
     @GetMapping("/fragments/general")
-    public String header(Model model) {
-        if (!model.containsAttribute("usuarioId")) {
-            model.addAttribute("usuarioId", null);
-        }
-        if (!model.containsAttribute("tipoUsuario")) {
-            model.addAttribute("tipoUsuario", null);
-        }
-        return "fragments/general.html";
+    public String header() {
+        return "/fragments/general.html";
     }
 }
