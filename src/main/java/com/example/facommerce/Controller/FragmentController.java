4bda9ad1ce461084aba2a5746782b38588a3d152
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-23 22:36:37 -0400

    Atualização para passar o usuarioId e tipoUsuario em FragmentController

--- src/main/java/com/example/facommerce/Controller/FragmentController.java
+++ src/main/java/com/example/facommerce/Controller/FragmentController.java
@@ -2,12 +2,15 @@ package com.example.facommerce.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-
+@SessionAttributes({"usuarioId", "tipoUsuario"})
 public class FragmentController {
+
     @GetMapping("/fragments/general")
-    public String header() {
+    public String header(@SessionAttribute("usuarioId") String usuarioId, @SessionAttribute("tipoUsuario") String tipoUsuario) {
         return "/fragments/general.html";
     }
 }
